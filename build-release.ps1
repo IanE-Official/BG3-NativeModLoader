10db5814e9263c02b93771d856cabf8ee53c8bd8
Ian E <iane@iane.dev>
2024-01-16 00:20:22 -0800

    Updated project name and incorporated vcpkg extern

--- build-release.ps1
+++ build-release.ps1
@@ -1,3 +1,3 @@
 Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
-& cmake -B $PSScriptRoot/build -S $PSScriptRoot --preset=REL -DPLUGIN_MODE:BOOL=TRUE
+& cmake -B $PSScriptRoot/build -S $PSScriptRoot --preset=REL -DPLUGIN_MODE:BOOL=TRUE -DCMAKE_TOOLCHAIN_FILE="$PSScriptRoot/extern/vcpkg/scripts/buildsystems/vcpkg.cmake"
 & cmake --build $PSScriptRoot/build --config Release
